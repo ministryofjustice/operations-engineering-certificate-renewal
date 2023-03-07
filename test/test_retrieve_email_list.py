f530debb9a227e7bba0bf22f4e20c5504b43cf84
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-07 16:03:07 +0000

    Additional coverage added for multiple emails and cname presence

--- test/test_retrieve_email_list.py
+++ test/test_retrieve_email_list.py
@@ -4,34 +4,12 @@ from scripts import certificate_expiry_check
 
 
 class TestCertificateExpiry(unittest.TestCase):
-
-    @patch('scripts.certificate_expiry_check.retrieve_email_list')
-    def test_build_parameters_returns_expected_data(self, mock_retrieve_email_list):
-
-        test_data = {
-            "domain": "www.test.com",
-            "days": 24,
-            "test_list": {'test': 'test'},
-            "test_email_addresses": ['test@digital.justice.gov.uk']
-        }
-
-        mock_retrieve_email_list.return_value = test_data['test_email_addresses']
-
-        result = certificate_expiry_check.build_params(
-            test_data['domain'], test_data['days'], test_data['test_list']
-        )
-
-        self.assertEqual(result['domain_name'], test_data['domain'])
-        self.assertEqual(result['days'], test_data['days'])
-        self.assertEqual(result['email_addresses'],
-                         test_data['test_email_addresses'])
-
     def test_matching_domain_returns_correct_data(self):
         test_data = {
             "test_list": {
                 "matching.domain.gov.uk": {
-                    "recipient": "test_user@mail.com",
-                    "recipientcc": "",
+                    "recipient": "test.user@mail.com",
+                    "recipientcc": [],
                     "owner": "OE",
                     "external_cname": []
                 }
@@ -51,8 +29,8 @@ class TestCertificateExpiry(unittest.TestCase):
         test_data = {
             "test_list": {
                 "matching.domain.gov.uk": {
-                    "recipient": "test_user@mail.com",
-                    "recipientcc": "",
+                    "recipient": "test.user@mail.com",
+                    "recipientcc": [],
                     "owner": "OE",
                     "external_cname": []
                 }
@@ -66,6 +44,54 @@ class TestCertificateExpiry(unittest.TestCase):
         )
 
         self.assertFalse(result)
+
+    def test_external_cname_returned_when_present(self):
+        test_data = {
+            "test_list": {
+                "matching.domain.gov.uk": {
+                    "recipient": "test.user@mail.com",
+                    "recipientcc": [],
+                    "owner": "OE",
+                    "external_cname": ["external.person@mail.com"]
+                }
+            },
+            "test_domain": "matching.domain.gov.uk"
+        }
+
+        result = certificate_expiry_check.retrieve_email_list(
+            test_data['test_domain'],
+            test_data['test_list']
+        )
+
+        self.assertEqual(result[0], test_data['test_list'][test_data['test_domain']]['external_cname'][0])
+
+    def test_matching_domain_returns_multiple_emails(self):
+        test_data = {
+            "test_list": {
+                "matching.domain.gov.uk": {
+                    "recipient": "test.user@mail.com",
+                    "recipientcc": [
+                        "another.user@mail.com",
+                        "yet.another.user@mail.com"
+                    ],
+                    "owner": "OE",
+                    "external_cname": []
+                }
+            },
+            "test_domain": "matching.domain.gov.uk"
+        }
+
+        result = certificate_expiry_check.retrieve_email_list(
+            test_data['test_domain'],
+            test_data['test_list']
+        )
+
+        self.assertEqual(result[0], test_data['test_list']
+                         [test_data['test_domain']]['recipient'])
+        self.assertEqual(result[1], test_data['test_list']
+                         [test_data['test_domain']]['recipientcc'][0])
+        self.assertEqual(result[2], test_data['test_list']
+                         [test_data['test_domain']]['recipientcc'][1])
 
 
 if __name__ == '__main__':
