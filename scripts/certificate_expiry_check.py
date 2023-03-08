f6226276a5e7d01e3434a6229a25c92794f66289
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-08 10:45:53 +0000

    Small rejiggle

--- scripts/certificate_expiry_check.py
+++ scripts/certificate_expiry_check.py
@@ -88,8 +88,8 @@ def build_params(domain_name: str, days: int, the_big_list):
 def retrieve_email_list(domain: str, the_big_list):
     filtered_email_list = []
     domain_exists_and_owned_by_ops_eng = domain in the_big_list and the_big_list[domain]["owner"] == "OE"
-    domain_contains_external_cname = the_big_list[domain]['external_cname'] is not None
     if domain_exists_and_owned_by_ops_eng:
+        domain_contains_external_cname = the_big_list[domain]['external_cname'] is not None
         if domain_contains_external_cname:
             for email in the_big_list[domain]['external_cname']:
                 filtered_email_list.append(email)
@@ -99,7 +99,7 @@ def retrieve_email_list(domain: str, the_big_list):
         for email in the_big_list[domain]["recipientcc"]:
             filtered_email_list.append(email)
         return filtered_email_list
-    return False
+    return []
 
 
 def send_email(email_type, params):
