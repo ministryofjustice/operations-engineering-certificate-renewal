164393afc0091241ba973a1c3a9c872ce91780d1
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-10 11:51:19 +0000

    Added run script as single entry point

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $1 = "unittest" ]
+then
+  export CONFIG_CONTEXT="./configs/test.yml"
+  python -m unittest discover -s app/test/ -fv
+  exit 0
+elif [ $1 = "testrun" ]
+# This is a temporary option for testing
+then
+  export CONFIG_CONTEXT="./configs/production.yml"
+  python3 app/send_email_test.py
+  exit 0
+elif [ $1 = "certexpire" ]
+then
+  export CONFIG_CONTEXT="./configs/production.yml"
+  python3 app/certificate_expiry_check.py
+  exit 0
+else
+  echo "Run failed:" >&2
+fi
+
+
+
