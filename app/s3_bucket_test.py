7d7758f7e603567745aca7531cf29d04230454a2
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-14 15:49:12 +0000

    Changed file name structure

--- app/s3_bucket_test.py
+++ app/s3_bucket_test.py
@@ -7,13 +7,12 @@ config_location = os.getenv(
 config = parse_config(config_location)
 file_path = './app/resources/s3_email_mapping.json'
 
-file_path = os.path.join(os.getcwd(), 'resources', 'mappings.json')
+# file_path = os.path.join(os.getcwd(), 'resources', 'mappings.json')
 
 s3 = boto3.client('s3')
 s3.download_file('operations-engineering-certificate-email', 'mappings.json', file_path)
 
-resources_dir = os.path.join(os.getcwd(), 'resources')
-for root, dir, files in os.walk(resources_dir):
-    for filename in files:
-        print(f"Filename: {filename}")
-        
+# resources_dir = os.path.join(os.getcwd(), 'resources')
+# for root, dir, files in os.walk(resources_dir):
+#     for filename in files:
+#         print(f"Filename: {filename}")
