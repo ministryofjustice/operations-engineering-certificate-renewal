54b7fdfb1a159fc08a1a1e185d2064ee637d550c
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-14 14:32:16 +0000

    Testing different retrieval method

--- app/s3_bucket_test.py
+++ app/s3_bucket_test.py
@@ -9,6 +9,12 @@ file_path = './app/resources/s3_email_mapping.json'
 
 print(f"Temp secret: {config['s3']['temp_secret']}")
 
+# s3 = boto3.client('s3')
+# s3.download_file(config.get(
+#     config['s3']['bucket_name']), config['s3']['object_name'], file_path)
+
 s3 = boto3.client('s3')
-s3.download_file(config.get(
-    config['s3']['bucket_name']), config['s3']['object_name'], file_path)
+with open('mappings.json', 'wb') as file:
+    s3.download_fileobj('BUCKET_NAME', 'OBJECT_NAME', file)
+    
+    print(f"FILE: {file}")
