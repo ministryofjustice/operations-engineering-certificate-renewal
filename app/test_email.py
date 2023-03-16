86c471727d8fce938d4d3512786ef5b3a97835f0
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-16 15:24:26 +0000

    Changing to for loop

--- app/test_email.py
+++ app/test_email.py
@@ -11,20 +11,24 @@ config = parse_config(config_location)
 def send_email():
     notifications_client = NotificationsAPIClient(
         config['keys']['notify_api_key'])
-    try:
-        response = notifications_client.send_email_notification(
-            email_address=
-            'sam.pepper@digital.justice.gov.uk, connor.glynn@digital.justice.gov.uk, '
-            'nick.walters@digital.justice.gov.uk',
-            template_id='72cd9e80-cf0c-4504-bee8-3b1acc3f0f9b',
-            personalisation={
-                "first_name": "Name",
-                "test_var": "Test"
-            }
-        )
-    except requests.exceptions.HTTPError as api_key_error:
-        raise SystemExit(
-            f"You may need to export your Notify API Key:\n {api_key_error}")
+    emails = [
+        'sam.pepper@digital.justice.gov.uk',
+        'connor.glynn@digital.justice.gov.uk',
+        'nick.walters@digital.justice.gov.uk'
+    ]
+    for email in emails:
+        try:
+            response = notifications_client.send_email_notification(
+                email_address=email,
+                template_id='72cd9e80-cf0c-4504-bee8-3b1acc3f0f9b',
+                personalisation={
+                    "first_name": "Name",
+                    "test_var": "Test"
+                }
+            )
+        except requests.exceptions.HTTPError as api_key_error:
+            raise SystemExit(
+                f"You may need to export your Notify API Key:\n {api_key_error}")
     return response
 
 
