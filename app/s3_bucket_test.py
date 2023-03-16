c2c92fab2a3cd3112ab1df3cccd1a6465432ef77
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-16 09:51:47 +0000

    Changed around variables to use secrets and changed some names

--- app/s3_bucket_test.py
+++ app/s3_bucket_test.py
@@ -7,18 +7,9 @@ config_location = os.getenv(
 config = parse_config(config_location)
 file_path = './app/resources/s3_email_mapping.json'
 
-# file_path = os.path.join(os.getcwd(), 'resources', 'mappings.json')
-
 s3 = boto3.client('s3')
-# s3.download_file('operations-engineering-certificate-email', 'mappings.json', file_path)
-
 with open(file_path, 'wb') as file:
-    s3.download_fileobj('operations-engineering-certificate-email', 'mappings.json', file)
+    s3.download_fileobj(config['s3']['s3_bucket_name'], config['s3']['s3_object_name'], file)
 
 with open(file_path) as my_file:
     print(my_file.read())
-
-# resources_dir = os.path.join(os.getcwd(), 'resources')
-# for root, dir, files in os.walk(resources_dir):
-#     for filename in files:
-#         print(f"Filename: {filename}")
