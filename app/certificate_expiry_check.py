6379e2c7f87e7f586f6cc804e326fbfaf7e227ea
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-21 16:02:01 +0000

    Removed logic from main script

--- app/certificate_expiry_check.py
+++ app/certificate_expiry_check.py
@@ -6,52 +6,20 @@ from notifications_python_client.notifications import NotificationsAPIClient
 from pyaml_env import parse_config
 
 from app.services.S3Service import S3Service
+from app.services.GandiService import GandiService
 
 config_location = os.getenv(
     "CONFIG_CONTEXT", default="../configs/production.yml")
 config = parse_config(config_location)
 
 
-def find_expiring_certificates(email_list):
+def find_expiring_certificates(email_list, certificate_list):
     """
     Finds all certificates that are due to expire, and sends an email if they meet the criteria.
     """
-    headers = {'Authorization': 'ApiKey ' + config['keys']['gandi_api_key']}
-    params = {'per_page': 1000}
-
-    try:
-        certificate_list = requests.get(
-            url=config['urls']['gandi_base_url'] +
-            config['urls']['gandi_cert_url_extension'],
-            params=params, headers=headers)
-        certificate_list.raise_for_status()
-    except requests.exceptions.HTTPError as authentication_error:
-        raise SystemExit(
-            f"You may need to export your Gandi API key:\n {authentication_error}")
-    except TypeError as api_key_error:
-        raise TypeError(
-            f"Gandi API key does not exist or is in the wrong format:\n {api_key_error}")
-
-    domain_list = certificate_list.json()
-
-    for domain_item in domain_list:
-        if domain_validity_check(domain_item, email_list):
-            date = datetime.datetime.strptime(
-                domain_item['dates']['ends_at'], '%Y-%m-%dT%H:%M:%SZ').date()
-            for threshold in config['cert_expiry_thresholds']:
-                if date == (datetime.datetime.today() + datetime.timedelta(days=threshold)).date():
-                    send_email('expire', build_params(
-                        domain_item['cn'], email_list, date, config['reply_email']))
-                    break
 
 
-def domain_validity_check(item, email_list):
-    domain_exists_in_map = item['cn'] in email_list
-    domain_is_in_valid_state = item['status'] == 'valid'
-    if domain_exists_in_map and domain_is_in_valid_state:
-        domain_is_owned_by_ops_eng = email_list[item['cn']]["owner"] == "OE"
-        return domain_is_owned_by_ops_eng
-    return False
+
 
 
 def build_params(domain_name: str, email_list, date, reply_email: str):
@@ -98,10 +66,22 @@ def send_email(email_type, params):
 
 
 def main():
+    # Instantiate services
     s3_service = S3Service()
+    gandi_service = GandiService()
+
+    # Get a list of the email mappings from S3
     email_mappings = s3_service.get_json_file(config['s3']['s3_bucket_name'], config['s3']['s3_object_name'],
                                               './app/s3_email_mapping.json')
-    find_expiring_certificates(email_mappings)
+
+    # Get a list of the expired certificates from Gandi
+    certificate_list = gandi_service.get_expiring_certificates(config)
+
+    # Send emails for the expired certificates using Notify
+
+    # Send report email to OE
+
+    find_expiring_certificates(email_mappings, certificate_list)
 
 
 if __name__ == "__main__":
