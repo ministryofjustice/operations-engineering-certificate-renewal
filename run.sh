068ed20730035e21c8d6ab5a2e230cee10ab1470
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-03-22 15:30:48 +0000

    ♻️ Refactor unit test structure for better decoupling

--- run.sh
+++ run.sh
@@ -2,15 +2,12 @@
 
 if [ $1 = "unittest" ]
 then
-  export CONFIG_CONTEXT="./configs/test.yml"
-  python -m unittest discover -s app/test/ -fv
+  python -m unittest discover -s test/ -fv
+
 elif [ $1 = "certexpire" ]
 then
   export CONFIG_CONTEXT="./configs/production.yml"
-  python3 app/certificate_expiry_check.py
+  python3 app/jobs/certificate_expiry_check.py
 else
   echo "Run failed:" >&2
 fi
-
-
-
