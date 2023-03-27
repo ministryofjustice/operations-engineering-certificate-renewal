16001f0324482df206af3fa60e75dfb9b6d22678
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-27 16:14:34 +0100

    Created initial test data class

---
+++ test/resources/test_data.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+class TestData:
+    test_domain_name = "test.domain.gov.uk"
+
+    @classmethod
+    def generate_valid_email_list(cls):
+        return {
+            cls.test_domain_name: {
+                "recipient": "test.user@mail.com",
+                "recipientcc": [],
+                "owner": "OE",
+                "external_cname": ["external.person@mail.com"],
+            }
+        }
+
+    @classmethod
+    def generate_filtered_certificate_list_with_expiry_date(cls, days: int):
+        expiry_date = datetime.datetime.now() + datetime.timedelta(days=days)
+        return {cls.test_domain_name: {'expiry_date': expiry_date}}
+
+    @classmethod
+    def generate_gandi_certificate_date(cls, state: str):
+        return [{"cn": cls.test_domain_name, "status": state, "dates": {"ends_at": "2023-01-01T06:00:00Z"}}]
+
