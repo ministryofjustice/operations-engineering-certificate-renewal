74343a1ec2d14ffe7fb8dd060701e9b29a7dc439
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-03-28 15:32:59 +0100

    New tests added for email retrieval from valid certificate lists

--- test/services/test_gandi_service.py
+++ test/services/test_gandi_service.py
@@ -12,10 +12,59 @@ class TestGetExpiredCertificates(unittest.TestCase):
     def test_valid_certificate_list_returns_expected_domain(self):
         response = GandiService(test_config, 'api_key', 'base_url', 'url_ext') \
             .get_expired_certificates_from_valid_certificate_list(
-            TestData.generate_filtered_certificate_list_with_expiry_date(30),
+            TestData.generate_single_filtered_certificate_list_with_expiry_date(30),
             TestData.generate_single_email_list())
-        
+
         self.assertIn(TestData.test_domain_name_root, response)
+
+    def test_valid_certificate_list_returns_multiple_expected_domains(self):
+        test_case_count = 3
+        response = GandiService(test_config, 'api_key', 'base_url', 'url_ext') \
+            .get_expired_certificates_from_valid_certificate_list(
+            TestData.generate_multiple_filtered_certificate_list_with_expiry_date(30, test_case_count),
+            TestData.generate_multiple_email_list(test_case_count))
+
+        self.assertIn(f"{TestData.test_domain_name_root}0", response)
+        self.assertIn(f"{TestData.test_domain_name_root}1", response)
+        self.assertIn(f"{TestData.test_domain_name_root}2", response)
+
+    def test_valid_certificate_list_returns_single_expected_email(self):
+        response = GandiService(test_config, 'api_key', 'base_url', 'url_ext') \
+            .get_expired_certificates_from_valid_certificate_list(
+            TestData.generate_single_filtered_certificate_list_with_expiry_date(30),
+            TestData.generate_single_email_list())
+
+        self.assertIn(TestData.test_recipient_email_root,
+                      response[TestData.test_domain_name_root]['emails'])
+
+    def test_valid_certificate_list_returns_multiple_expected_emails(self):
+        response = GandiService(test_config, 'api_key', 'base_url', 'url_ext') \
+            .get_expired_certificates_from_valid_certificate_list(
+            TestData.generate_single_filtered_certificate_list_with_expiry_date(30),
+            TestData.generate_single_email_list(recipcc=3))
+
+        self.assertIn(TestData.test_recipient_email_root,
+                      response[TestData.test_domain_name_root]['emails'])
+        self.assertIn(f"{TestData.test_recipientcc_email_root}{0}",
+                      response[TestData.test_domain_name_root]['emails'])
+        self.assertIn(f"{TestData.test_recipientcc_email_root}{1}",
+                      response[TestData.test_domain_name_root]['emails'])
+        self.assertIn(f"{TestData.test_recipientcc_email_root}{2}",
+                      response[TestData.test_domain_name_root]['emails'])
+
+    def test_valid_certificate_list_returns_expected_cname(self):
+        test_case_count = 3
+        response = GandiService(test_config, 'api_key', 'base_url', 'url_ext') \
+            .get_expired_certificates_from_valid_certificate_list(
+            TestData.generate_single_filtered_certificate_list_with_expiry_date(30),
+            TestData.generate_single_email_list(cname=test_case_count))
+
+        self.assertIn(f"{TestData.test_cname_email_root}{0}",
+                      response[TestData.test_domain_name_root]['emails'])
+        self.assertIn(f"{TestData.test_cname_email_root}{1}",
+                      response[TestData.test_domain_name_root]['emails'])
+        self.assertIn(f"{TestData.test_cname_email_root}{2}",
+                      response[TestData.test_domain_name_root]['emails'])
 
 
 @freeze_time("2023-01-01")
