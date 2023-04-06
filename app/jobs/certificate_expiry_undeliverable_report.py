c32fc3dd8720748bb825850204262b60b3adb733
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-04-06 12:12:37 +0100

    Added separate run for checking for undelivered emails

---
+++ app/jobs/certificate_expiry_undeliverable_report.py
@@ -0,0 +1,35 @@
+import sys
+
+from app.config import config
+from app.services.NotifyService import NotifyService
+
+
+def main(testrun: bool = False, test_email: str = ""):
+    notify_service = NotifyService(
+        config,
+        config['keys']['notify_api_key'])
+
+    print("Building undelivered email report...")
+    undelivered_email_report = notify_service.check_for_undelivered_emails_for_template(
+        config['template_ids']['cert_expiry'])
+
+    if testrun:
+        print(f"Sending test undelivered email report to {test_email}...")
+        notify_service.send_report_email_to_operations_engineering(
+            notify_service.build_undeliverable_email_report_string(
+                undelivered_email_report), config['template_ids']['undelivered_report'], test_email)
+    else:
+        print("Sending live report to Operations Engineering...")
+        notify_service.send_report_email_to_operations_engineering(
+            notify_service.build_undeliverable_email_report_string(
+                undelivered_email_report), config['template_ids']['undelivered_report'], config['reply_email'])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == '-test':
+        if len(sys.argv) > 2:
+            main(True, sys.argv[2])
+        else:
+            raise SystemExit('Email address of recipient expected.')
+    else:
+        main()
